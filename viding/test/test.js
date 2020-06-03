a4f33e3b52568ee386d153ce6b343b35583d5cf1
Viding91 <irvingma91@gmail.com>
2020-06-03 18:51:52 -0500

    Added new folder and HelloWorld test

---
+++ viding/test/test.js
@@ -0,0 +1,12 @@
+(function(){
+    "use strict";
+    /* Start of your code */
+    function greetMe(yourName) {
+      /*alert('Hello ' + yourName);*/
+      console.log('Hello' + yourName);
+    }
+    
+    greetMe('World');
+    console.log('test');
+    /* End of your code */
+  })();
